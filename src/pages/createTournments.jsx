be39995eb8a1c70bffaa962be8b6eb12836184c0
hamza-gaa <127301261+hamza-gaa@users.noreply.github.com>
2023-04-03 13:07:34 +0200

    Update createTournments.jsx
    Create true_false competition

--- src/pages/createTournments.jsx
+++ src/pages/createTournments.jsx
@@ -3,11 +3,20 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Button, Container, Form, Row, Col, FormLabel } from "react-bootstrap";
-const baseURL = "http://localhost:5000/api/v1/tournament";
+const baseURL = "http://localhost:5000/api/v1/createCompetitionTrueFalse/";
 function CreateTournments() {
   console.log(new Date().getTime());
   const [title, setTitle] = useState("");
   const [description, setDescrption] = useState("");
+  const [theme, setTheme] = useState("");
+  const [q1, setQ1] = useState("");
+  const [a1, setA1] = useState("");
+  const [q2, setQ2] = useState("");
+  const [a2, setA2] = useState("");
+  const [q3, setQ3] = useState("");
+  const [a3, setA3] = useState("");
+  const [q4, setQ4] = useState("");
+  const [a4, setA4] = useState("");
   const [message, setMessage] = useState("");
   const [joinRequired, setJoinRequired] = useState("");
   const [subject, setSubject] = useState("");
@@ -17,14 +26,27 @@ function CreateTournments() {
   const [startTime, setStartTime] = useState(
     Math.floor(new Date().getTime() / 1000)
   );
+  const token = localStorage.getItem("jwt");
   const [governorate, setGovernorate] = useState("Giza");
   const [endTime, setEndTime] = useState(0);
+
+
+  const [fields, setFields] = useState({});
+  function handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    
+    setFields({...fields, [name]: value})
+    console.log(fields['revenueGenerated'])
+}
 
   const navigate = useNavigate();
   let handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      let res = await axios.post(baseURL, {
+      let res = await axios.post(baseURL,
+        {
         notification: {
           subject: "New tournment",
           content: {
@@ -38,6 +60,7 @@ function CreateTournments() {
           location: governorate,
         },
         tournament: {
+          id:"",
           authoritative: false,
           sortOrder,
           operator,
@@ -55,6 +78,21 @@ function CreateTournments() {
           maxNumScore: 2,
           joinRequired: true,
         },
+        objects: [
+          {
+          collection: theme,
+          key: "",
+          value: `{"questions": [{"question": "${q1}","answer": ${a1}},{"question": "${q2}","answer": ${a2}},{"question": "${q3}","answer": ${a3}},{"question": "${q4}","answer": ${a4}}]}`,
+          version: null,
+          permissionWrite: 1,
+          permissionRead: 2
+          }
+      ]
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
       console.log(res);
       if (res.status === 200) {
@@ -70,6 +108,7 @@ function CreateTournments() {
       console.log(err);
     }
   };
+
 
   return (
     <div className="App">
@@ -108,15 +147,112 @@ function CreateTournments() {
             </Form.Group>
           </Col>
           <Col>
-            <Form.Group className="mb-3" controlId="Subject">
+            <Form.Group className="mb-3" controlId="theme">
               <FormLabel>Theme</FormLabel>
+              <Form.Control type="text" as="select" placeholder="Theme" name="tuornament-type"
 
-              <Form.Control type="text" as="select" placeholder="operator">
-                <option value="ascending"> True and False </option>
+                    onChange={ e=> { handleChange(e) ;setTheme(e.target.value) } }
+                    value={fields["tuornament-type"]}>
+                <option value="true_false"> True and False </option>
+                <option value="points"> Pruches </option>
+                <option value="mcq"> Pruches </option>
               </Form.Control>
             </Form.Group>
           </Col>
         </Row>
+
+        {
+
+          fields['tuornament-type'] === 'true_false' ?
+         <>
+         <Row>
+              <Col>
+                <Form.Group className="mb-3" controlId="question">
+                  <FormLabel>Questions</FormLabel>
+                  <Form.Control type="text" placeholder="question" onChange={(e) => setQ1(e.target.value)}>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="answer">
+                  <FormLabel>Answer</FormLabel>
+                  <select name="revenueGenerated"
+                    onChange={e => setA1(e.target.value)}
+                    value={fields["revenueGenerated"]}
+                  >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                  </select>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+                <Col>
+                  <Form.Group className="mb-3" controlId="question">
+                    <FormLabel>Questions</FormLabel>
+                    <Form.Control type="text" placeholder="question" onChange={(e) => setQ2(e.target.value)}>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group className="mb-3" controlId="answer">
+                    <FormLabel>Answer</FormLabel>
+                    <select name="revenueGenerated"
+                      onChange={e => setA2(e.target.value)}
+                      value={fields["revenueGenerated"]}
+                    >
+                      <option value="true">True</option>
+                      <option value="false">False</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+              </Row>
+         <Row>
+              <Col>
+                <Form.Group className="mb-3" controlId="question">
+                  <FormLabel>Questions</FormLabel>
+                  <Form.Control type="text" placeholder="question" onChange={(e) => setQ3(e.target.value)}>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="answer">
+                  <FormLabel>Answer</FormLabel>
+                  <select name="revenueGenerated"
+                    onChange={e => setA3(e.target.value)}
+                    value={fields["revenueGenerated"]}
+                  >
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                  </select>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+                <Col>
+                  <Form.Group className="mb-3" controlId="question">
+                    <FormLabel>Questions</FormLabel>
+                    <Form.Control type="text" placeholder="question" onChange={(e) => setQ4(e.target.value)}>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group className="mb-3" controlId="answer">
+                    <FormLabel>Answer</FormLabel>
+                    <select name="revenueGenerated"
+                      onChange={e => setA4(e.target.value)}
+                      value={fields["revenueGenerated"]}
+                    >
+                      <option value="true">True</option>
+                      <option value="false">False</option>
+                    </select>
+                  </Form.Group>
+                </Col>
+              </Row>
+              </>
+         :null
+        }
+
         <Row>
           <Col xs={5}>
             <Form.Group className="mb-3" controlId="sortOrder">
